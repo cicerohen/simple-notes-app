920d597640d048a47e243c11673bd28df29f445a
Cícero Viana <cicerohen@gmail.com>
2019-10-30 20:56:23 -0300

    improving babel/jest/eslint configs

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  setupFilesAfterEnv: [
-    '<rootDir>/__test__/setup/setup-react-testing-library.js'
-  ],
-  setupFiles: ['<rootDir>/__test__/setup/setup-env.js'],
+  setupFilesAfterEnv: ['<rootDir>/__test__/setup/setup-files-after-env.js'],
+  setupFiles: ['<rootDir>/__test__/setup/setup-files.js'],
   testPathIgnorePatterns: ['<rootDir>/__test__/setup/']
 };
