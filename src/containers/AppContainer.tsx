a75170fe6a3fd0542eb1ead9edc8a1d2163d456a
Cícero Viana <cicerohen@gmail.com>
2019-11-03 13:07:49 -0300

    adds initial route configs

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -1,33 +1,17 @@
 import React, { FC } from 'react';
-import styled from 'styled-components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from '../theme';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Grid from '../components/Grid';
-import Card from '../components/Card';
-
-const MainContent = styled.main`
-  min-height: 800px;
-`;
+import HomeContainer from './pages/HomeContainer';
 
 const App: FC = () => (
   <ThemeProvider>
-    <div>
-      <Header />
-      <MainContent>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column medium={6}>
-              <Card>6 COL</Card>
-            </Grid.Column>
-            <Grid.Column medium={6}>
-              <Card>6 COL</Card>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </MainContent>
-      <Footer />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <HomeContainer />
+        </Route>
+      </Switch>
+    </Router>
   </ThemeProvider>
 );
 
---
+++ src/containers/pages/HomeContainer.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import View from '../../components/View';
+
+const HomeContainer: FC = () => (
+  <View>
+    <div>Home page</div>
+  </View>
+);
+
+export default HomeContainer;
