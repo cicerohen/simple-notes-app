a5e2bbaa4e3467dc32c09628e322d559f69f9f88
Cícero Viana <cicerohen@gmail.com>
2019-11-03 11:43:42 -0300

    adds a view component

---
+++ src/components/View/View.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import Header from '../Header';
+import { DEFAULT_PADDING } from '../../theme';
+
+const Wrapper = styled.div``;
+
+const MainContent = styled.main`
+  min-height: 400px;
+  position: relative;
+  padding-top: ${DEFAULT_PADDING};
+  padding-left: ${DEFAULT_PADDING};
+  padding-right: ${DEFAULT_PADDING};
+  max-width: 1020px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  text-transform: uppercase;
+  color: ${props => props.theme.palette.primaryColor};
+`;
+
+const Subtitle = styled.h2`
+  color: ${props => props.theme.palette.secondaryColor};
+`;
+
+interface ViewPropsInterface {
+  title?: object | Array<object> | string;
+  subtitle?: object | Array<object> | string;
+  children?: object | Array<object>;
+}
+
+const View: FC<ViewPropsInterface> = ({
+  title,
+  subtitle,
+  children
+}: ViewPropsInterface) => {
+  return (
+    <Wrapper>
+      <Header />
+      <MainContent>
+        {title && <Title>{title}</Title>}
+        {subtitle && <Subtitle>{subtitle}</Subtitle>}
+        {children}
+      </MainContent>
+    </Wrapper>
+  );
+};
+
+export default View;
