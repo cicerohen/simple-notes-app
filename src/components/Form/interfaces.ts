4542d5918e6648169e49aa3c388aa26e527b4ed3
Cicero Viana <cicerohen@gmail.com>
2019-12-28 17:33:18 -0300

    Removes Form component

--- src/components/Form/interfaces.ts
+++
@@ -1,9 +0,0 @@
-import { FormEvent } from 'react';
-
-export interface InputPropsInterface {
-  type?: string;
-  placeholder?: string;
-  className?: string;
-  value?: string | number;
-  onChange?: (event: FormEvent<HTMLInputElement>) => void;
-}
