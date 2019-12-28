5ae7a6435b892ef5bc5380569a9025b3fbf8bf3c
Cicero Viana <cicerohen@gmail.com>
2019-12-28 17:29:06 -0300

    Adds Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { DEFAULT_PADDING } from '../../theme';
+import { InputPropsInterface } from './interfaces';
+
+const Wrapper = styled.input`
+  min-height: 30px;
+  padding-left: ${DEFAULT_PADDING};
+  padding-right: ${DEFAULT_PADDING};
+`;
+
+const Input: FC<InputPropsInterface> = ({
+  type = 'text',
+  placeholder,
+  className,
+  value,
+  onChange
+}: InputPropsInterface) => (
+  <Wrapper
+    type={type}
+    placeholder={placeholder}
+    className={className}
+    value={value}
+    onChange={onChange}
+  />
+);
+
+export default Input;
