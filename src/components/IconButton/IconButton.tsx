b1c581a0a7612a943b3fb8c16b5ced99c60398fe
Cicero Viana <cicerohen@gmail.com>
2019-12-28 20:22:34 -0300

    Adjusts IconButton component

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -3,19 +3,39 @@ import styled from 'styled-components';
 import Button from '../Button';
 import { IconButtonPropsInterface } from './interfaces';
 
-const Wrapper = styled(Button).attrs(() => ({
-  size: 'large'
-}))`
+const Wrapper = styled(Button).attrs(() => ({}))`
   padding: 0.5em;
   border-radius: 50%;
+  ${props =>
+    props.size === 'small' &&
+    `
+    font-size: 1.4rem;
+  `};
+  ${props =>
+    props.size === 'medium' &&
+    `
+    font-size: 1.6rem;
+  `};
+  ${props =>
+    props.size === 'large' &&
+    `
+    font-size: 2.5rem;
+  `};
 `;
 
 const IconButton: FC<IconButtonPropsInterface> = ({
   component,
   variant,
-  className
+  size = 'small',
+  className,
+  onClick
 }: IconButtonPropsInterface) => (
-  <Wrapper variant={variant} className={className}>
+  <Wrapper
+    variant={variant}
+    size={size}
+    className={className}
+    onClick={onClick}
+  >
     {component({})}
   </Wrapper>
 );
