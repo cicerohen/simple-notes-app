8f1ac8ae484579e2937c284a63b79daad696f542
Cicero Viana <cicerohen@gmail.com>
2019-12-30 00:52:18 -0300

    Adds a simple hook to handle portal ref

---
+++ src/hooks/usePortal.tsx
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from 'react';
+
+type usePortalType = (id: string) => any;
+
+const usePortal: usePortalType = id => {
+  const container = useRef(
+    document.querySelector(`#${id}`) || document.createElement('div')
+  );
+
+  useEffect(() => {
+    container.current.id = id;
+    document.body.appendChild(container.current);
+    return () => container.current && container.current.remove();
+  }, []);
+
+  return {
+    container
+  };
+};
+
+export default usePortal;
